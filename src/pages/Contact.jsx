97b4bc75f94811bd8f59c6ce0cca3450fea8cd44
Jailene <jaileneveras8@gmail.com>
2023-03-15 16:14:05 -0400

    made contact form submittable

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,30 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import CreateIcon from '@mui/icons-material/Create';
 import "../styles/contact.css"
+import { Cat } from 'react-kawaii'
 
 function Contact(){
-    return (<div className= "contactForm">  
-       <h1> Contact Me</h1>
-       <form >
-        <input placeholder="Name"
-        type="text"
-        />
-        <br></br>
-        <input placeholder ="email"
-        type= "text"
-    />
-    <br></br>
-        <textarea className="message" placeholder = "message" 
-        type = "text"
-        />        
-        <br></br>
-
-      
-
-       <button classname= "Submit"> Submit </button>
-       </form>
-
-    </div>)
+    const [showImage, setShowImage] = useState(false);
+    const [showForm, setShowForm] = useState(true);
+    const handleButtonClick = (event) => {
+        event.preventDefault();
+        setShowImage(true);
+        setShowForm(false);
+    }
+    return (
+        <div className= "contactForm">  
+            {showForm &&
+                <>
+                    <h1> Contact Me</h1>
+                    <form>
+                        <input placeholder="Name" type="text" />
+                        <br></br>
+                        <input placeholder ="email" type= "text" />
+                        <br></br>
+                        <textarea className="message" placeholder="message" type="text" />
+                        <br></br>
+                        <button onClick={handleButtonClick} className="Submit"> Submit </button>
+                    </form>
+                </>
+            }
+            {showImage &&<> <Cat size={320} mood="excited" color="#596881"  />
+            <div>"Thank you for your message! I shall contact you as soon as I can"</div> </>} 
+        </div>
+    );
 }
 
 export default Contact;
